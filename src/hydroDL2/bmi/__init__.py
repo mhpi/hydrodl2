6854f0f7e912fe9f43fd1ab9c5f260e47b135777
Leo Lonzarich <lgl5139@psu.edu>
2025-02-16 13:27:09 -0600

    remove bmi dev

--- src/hydroDL2/bmi/__init__.py
+++
@@ -1,4 +0,0 @@
-# Temporarily import dPLHydro_multimodel package (PMI) until BMI package is
-# self-complete.
-
-import hydro_multimodel.dPLHydro_multimodel as pmi
--- src/hydroDL2/bmi/__main__.py
+++
@@ -1,4 +0,0 @@
-from extern.dpl_model_package.run_dpl_model_bmi import execute
-
-if __name__ == '__main__':
-    execute()
