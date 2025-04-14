190b66472ad65980af48f6ec0e9dc3b3305b5684
Leo Lonzarich <lgl5139@psu.edu>
2025-04-14 14:39:57 -0400

    Update __init__.py
    Temporary fix for colab.

--- src/hydroDL2/__init__.py
+++ src/hydroDL2/__init__.py
@@ -1,10 +1,10 @@
-from hydroDL2._version import __version__
+# from hydroDL2._version import __version__
 from hydroDL2.api.methods import (available_models, available_modules,
                                   load_model, load_module)
 from hydroDL2.models import *
 
 # In case setuptools scm says version is 0.0.0
-assert not __version__.startswith("0.0.0")
+# assert not __version__.startswith("0.0.0")
 
 # List of all modules to be accessible accross subpackage.
 __all__ = ['available_models',
